93e931586c93165752a73e198eb05292d1ac0932
ghankeliu2015 <hanke.liu@gmail.com>
2019-04-10 14:52:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName= 'bob'
+
+function upperCaseCustomerName () {
+  customerName = customerName.toUpperCase();
+  // return customerName;
+}
+
+function setBestCustomer () {
+  bestCustomer = "not bob"
+}
+
+var bestCustomer
+
+function  overwriteBestCustomer () {
+  bestCustomer = "maybe bob"  //want it to be global
+}
+
+const leastFavoriteCustomer = 123
+
+function changeLeastFavoriteCustomer () {
+  leastFavoriteCustomer = 456
+}
